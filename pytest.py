536f7abc1e9592fa27610bd90d82837e44b29d3c
liuchi2185 <liuchi2185@163.com>
2019-11-14 16:19:59 +0800

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,36 +1,11 @@
-from urllib import request,parse
-import urllib
-import http.cookiejar
+import requests
 from bs4 import BeautifulSoup
 
-word=input("请输入搜索的关键词:");
-url="http://www.baidu.com/s?wd="+urllib.parse.quote(word);
+url="http://www.baidu.com/s?wd=刘驰"
+r = requests.get(url)
+soup=BeautifulSoup(r.text,'html.parser')
 
-headers={"Accept": "text/html, application/xhtml+xml, image/jxr, */*",
-
-         "Accept - Encoding": "gzip, deflate, br",
-
-         "Accept - Language": "zh - CN",
-
-         "Connection": "Keep - Alive",
-         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
-         "referer":"baidu.com"};
-cjar = http.cookiejar.CookieJar();
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cjar));
-
-headall=[];
-for key,value in headers.items():
-    item=(key,value);
-    headall.append(item);
-opener.addheaders=headall;
-urllib.request.install_opener(opener);
-data =urllib.request.urlopen(url).read().decode('utf-8');
-soup=BeautifulSoup(data,'html.parser');
-# 以格式化的形式打印html
-#print(soup.prettify());
 for result_table in soup.find_all('h3',class_='t'):
-
-    a_click = result_table.find("a");
-
-    print( "-----标题----\n" + a_click.get_text())  # 标题
-    print("----链接----\n" + str(a_click.get("href")))  # 链接
+    a_click = result_table.find("a")
+    print( "-----标题----\n" + a_click.get_text())
+    print("----链接----\n" + str(a_click.get("href")))
